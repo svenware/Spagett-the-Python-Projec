a7c6a42b9a06d1d627f24c5cb75df2f99990bf12
Ivan Smith <ivan.smith1@students.mq.edu.au>
2020-10-02 19:08:59 +1000

    Added python files

---
+++ Group Assignment/Group Assignment.py
@@ -0,0 +1,14 @@
+# Group Assignment
+
+
+def covid(days, time, compliance):
+    try:
+        data = open('data.txt', 'r')
+    except ValueError:
+        print('File not found.')
+        return
+
+
+covid()
+
+covid(10, 5, 2)
